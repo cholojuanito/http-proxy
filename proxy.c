913506ab4724cdfe8c04c3ffce7237d379057078
Tanner <tannerdavisr@byu.edu>
2019-03-18 18:36:38 -0600

    Able to get 78/97 with the driver
    Concurrency is not working correctly

--- proxy.c
+++ proxy.c
@@ -1,61 +1,65 @@
+/**
+ * Tanner Davis - BYU CS 324 Winter 2019
+ * 
+ */ 
 #include <stdio.h>
 
 //#include "cache.h"
 #include "csapp.h"
 
-/* Recommended max cache and object sizes */
+/* Recommended max cache and object contentLengths */
 #define MAX_CACHE_SIZE           1049000
 #define MAX_OBJECT_SIZE         102400
 #define MIN_PORT_NUMBER      1024
 #define MAX_PORT_NUMBER     65536
-#define CACHE_OFF                        "off"
-#define MULTILINE_FLAG               "/"
-#define DEFAULT_HTTP_PORT    "80"
-#define GET                                        "GET"
-#define END                                       "\r\n"
-#define USER_AGENT_KEY           "User-Agent:"
-#define ACCEPT_KEY                     "Accept:"
-#define ACCEPT_ENCODE_KEY "Accept-Encoding:"
+#define CACHE_OFF              "off"
+#define FORWARD_SLASH          "/"
+#define HTTP_DELIM             "://"
+#define END_STR                '\0'
+#define DEFAULT_HTTP_PORT      "80"
+#define GET                    "GET"
+#define END_HTTP               "\r\n"
+#define HOST_KEY               "Host:"
+#define USER_AGENT_KEY         "User-Agent:"
+#define ACCEPT_KEY             "Accept:"
+#define ACCEPT_ENCODE_KEY      "Accept-Encoding:"
 #define CONNECTION_KEY         "Connection:"
-#define PROXY_KEY                       "Proxy Connection:"
-#define COOKIE_KEY                      "Cookie:"
-#define CONTENT_LEN_STR_1    "Content-Length"
-//#define CONTENT_LEN_KEY_1   "Content-Length:"
-#define CONTENT_LEN_STR_2   "Content-length"
-//#define CONTENT_LEN_KEY_2   "Content-length:"
+#define PROXY_KEY              "Proxy Connection:"
+#define COOKIE_KEY             "Cookie:"
+#define CONTENT_LEN_STR_1      "Content-Length"
+//#define CONTENT_LEN_KEY_1    "Content-Length:"
+#define CONTENT_LEN_STR_2      "Content-length"
+//#define CONTENT_LEN_KEY_2    "Content-length:"
 
 // Headers
 static const char* userAgentHeadr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 static const char* connectionHeadr = "Connection: close\r\n";
 static const char* proxyConnectionHeadr = "Proxy-Connection: close\r\n";
 static const char* httpVersionHeadr = "HTTP/1.0\r\n";
-static const char* dnsFailStr = "HTTP/1.0 400 \
-    Bad Request\r\nServer: RUI_Proxy\r\nContent-Length: 137\r\nConnection: \
-    close\r\nContent-Type: text/html\r\n\r\n<html><head></head><body><p>\
-    This server coulnd't be connected, because DNS lookup failed.</p><p>\
-    Powered by Rui Zhang.</p></body></html>";
-static const char* connectionFailStr = "HTTP/1.0 400 \
-    Bad Request\r\nServer: RUI_Proxy\r\nContent-Length: 108\r\nConnection: \
-    close\r\nContent-Type: text/html\r\n\r\n<html><head></head><body><p>\
-    This server coulnd't be connected.</p><p>\
-    Powered by Rui Zhang.</p></body></html>";
-
-
-//TODO: Make sure you need this
 static const char* acceptStr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char* acceptEncoding = "Accept-Encoding: gzip, deflate\r\n";
+static const char* dnsFailStr = "HTTP/1.0 400 \
+    Bad Request\r\nServer: CS324-Proxy\r\nContent-Length: 0\r\nConnection: \
+    close\r\nContent-Type: text/html\r\n\r\n";
+    /* <html><head></head><body><p>\
+     Couldn't connect with this server because of DNS issues.</p></body></html> */
+static const char* connectionFailStr = "HTTP/1.0 400 \
+    Bad Request\r\nServer: CS324-Proxy\r\nContent-Length: 0\r\nConnection: \
+    close\r\nContent-Type: text/html\r\n\r\n";
+    /* <html><head></head><body><p>\
+    Couldn't connect with this server.</p></body></html> */
 
 // Function declarations
 void usage();
 void proxyProcess(int* arg);
-int readRequest(char* str, int clientFd, char* host, char* port, char* cacheIdx, char* resource);
+int readRequest(char* str, int clientFd, char* host, char* port, char* cacheIdx, char* query);
 int forwardToServer(char* host, char* port, int* serverFd, char* requestStr);
-int forwardToClient(int clientFd, char* content, unsigned int len);
-int readAndForwardResponse(int serverFd, int clientFd, char* cacheIdx, char* content);
+int forwardToClient(int clientFd, char* response, unsigned int len);
+int readAndForwardToClient(int serverFd, int clientFd, char* cacheIdx, char* response);
 
-int append(char* content, char* str, unsigned int addSize, unsigned int* prevSize);
-int parseRequest(char* str, char* method, char* protocol, char* version, char* hostPort, char* resource);
-void getHostAndPort(char* hostPort, char* host, char* port);
+int append(char* response, char* str, unsigned int addSize, unsigned int* prevSize);
+int parseRequest(char* buf, char* method, char* protocol, char* version, char* hostAndPort, char* query);
+void getHostAndPort(char* hostAndPort, char* host, char* port);
 void closeFd(int* clientFd, int* serverFd);
 // END function declarations
 
@@ -96,27 +100,6 @@ int main(int argc, char** argv) {
     // Ignore SIGPIPE
     Signal(SIGPIPE, SIG_IGN);
 
-    // ip4addr.sin_family = AF_INET;
-    // ip4addr.sin_port = htons(atoi(argv[1]));
-    // ip4addr.sin_addr.s_addr = INADDR_ANY;
-
-    // if ((listenFd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    //     perror("socket error");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // if (bind(listenFd, (struct sockaddr*)&ip4addr, sizeof(struct sockaddr_in)) < 0) {
-    //     close(listenFd);
-    //     perror("bind error");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // if (listen(listenFd, 100) < 0) {
-    //     close(listenFd);
-    //     perror("listen error");
-    //     exit(EXIT_FAILURE);
-    // }
-
     listenFd = Open_listenfd(port);
     while (1) {
         pthread_t tid;
@@ -147,16 +130,16 @@ void proxyProcess(int* arg) {
 	  Pthread_detach(Pthread_self());
 
     int clientFd = (int)(*arg);
-	  int serverFd = -1;
-    char buf[MAXBUF], requestStr[MAXBUF], host[MAXBUF], port[MAXBUF], resource[MAXBUF];
-    char cacheIdx[MAXBUF], content[MAXBUF];
+	int serverFd = -1;
+    char buf[MAXBUF], requestStr[MAXBUF], host[MAXBUF], port[MAXBUF], query[MAXBUF];
+    char cacheIdx[MAXBUF], response[MAX_CACHE_SIZE];
     unsigned int length;
 
     // Read the client request
-    int readVal = readRequest(requestStr, clientFd, host, port, cacheIdx, resource);
+    int readVal = readRequest(requestStr, clientFd, host, port, cacheIdx, query);
 
     printf("Read data from %s:%s\n",host,port);
-	  fflush(stdout);
+	fflush(stdout);
 
     if (!readVal) {
         // if (!read_node_content()) {
@@ -167,33 +150,33 @@ void proxyProcess(int* arg) {
         // }
 
         // Forward request to remote sever
-        int serverResponseVal = forwardToServer(host, port, &serverFd, requestStr);
-        if (serverResponseVal == -1) {
-            fprintf(stderr, "forward content to server error.\n");
+        int bytesWritten = forwardToServer(host, port, &serverFd, requestStr);
+        if (bytesWritten == -1) {
+            fprintf(stderr, "forward response to server error.\n");
             strcpy(buf, connectionFailStr);
             Rio_writen(clientFd, buf, strlen(connectionFailStr));
         }
-        else if (serverResponseVal == -2) {
-            fprintf(stderr, "forward content to server error (dns look up fail).\n");
+        else if (bytesWritten == -2) {
+            fprintf(stderr, "forward response to server error (dns look up fail).\n");
             strcpy(buf, dnsFailStr);
             Rio_writen(clientFd, buf, strlen(dnsFailStr));
         }
         else {
             //if (Rio_writen(server_fd, request_str, strlen(request_str)) == -1)
-            //	fprintf(stderr, "forward content to server error.\n");
+            //	fprintf(stderr, "forward response to server error.\n");
 
-            int fVal = readAndForwardResponse(serverFd, clientFd, cacheIdx, content);
+            int fVal = readAndForwardToClient(serverFd, clientFd, cacheIdx, response);
             if (fVal == -1)
-                fprintf(stderr, "forward content to client error.\n");
+                fprintf(stderr, "forward response to client error.\n");
             else if (fVal == -2)
-                fprintf(stderr, "save content to cache error.\n");
+                fprintf(stderr, "save response to cache error.\n");
         }
     }
 
 }
 
-int readRequest(char* request, int clientFd, char* host, char* port, char* cacheIdx, char* resource) {
-    char buf[MAXBUF], method[MAXBUF], protocol[MAXBUF], hostPort[MAXBUF], version[MAXBUF];
+int readRequest(char* request, int clientFd, char* host, char* port, char* cacheIdx, char* query) {
+    char buf[MAXBUF], method[MAXBUF], protocol[MAXBUF], hostAndPort[MAXBUF], version[MAXBUF];
      rio_t rioClient;
 
     Rio_readinitb(&rioClient, clientFd);
@@ -201,46 +184,53 @@ int readRequest(char* request, int clientFd, char* host, char* port, char* cache
         return -1;
     }
 
-    if (parseRequest(buf, method, protocol, hostPort, resource, version) == -1) {
+    if (parseRequest(buf, method, protocol, version, hostAndPort, query) == -1) {
         return -1;
     }
 
-    getHostAndPort(hostPort, host, port);
+    getHostAndPort(hostAndPort, host, port);
 
+    // Begin copying the request
+    // GET requests
     if (strstr(method, GET)) {
+
         strcpy(request, method);
         strcat(request, " ");
-        strcat(request, resource);
+        strcat(request, query);
         strcat(request, " ");
         strcat(request, httpVersionHeadr);
 
+        // Add Host information if present
         if (strlen(host)) {
-            strcpy(buf, "Host: ");
+            strcpy(buf, HOST_KEY);
             strcat(buf, host);
             strcat(buf, ":");
             strcat(buf, port);
-            strcat(buf, "\r\n");
+            strcat(buf, END_HTTP);
             strcat(request, buf);
         }
 
+        // Add necessary headers
         strcat(request, userAgentHeadr);
         strcat(request, acceptStr);
         strcat(request, acceptEncoding);
         strcat(request, connectionHeadr);
         strcat(request, proxyConnectionHeadr);
 
+
+        // Copy any other relavant headers
         while(Rio_readlineb(&rioClient, buf, MAXBUF) > 0) {
-            //
-            if (!strcmp(buf, END)) {
-                strcat(request, END);
+            // End when you see '\r\n'
+            if (!strcmp(buf, END_HTTP)) {
+                strcat(request, END_HTTP);
                 break;
             }
-            //
+            // Ignore these headers, we overwrote them
             else if (strstr(buf, USER_AGENT_KEY) || strstr(buf, ACCEPT_KEY) || strstr(buf, ACCEPT_ENCODE_KEY)
-                           || strstr(buf, PROXY_KEY) || strstr(buf, CONNECTION_KEY) || strstr(buf, COOKIE_KEY) ) {
+                    || strstr(buf, PROXY_KEY) || strstr(buf, CONNECTION_KEY) || strstr(buf, COOKIE_KEY)
+                    || strstr(buf, HOST_KEY)) {
                     continue;
             }
-            //
             else {
                 strcat(request, buf);
             }
@@ -250,30 +240,35 @@ int readRequest(char* request, int clientFd, char* host, char* port, char* cache
         // strcpy(cacheIdx, host);
         // strcat(cacheIdx, ":");
         // strcat(cacheIdx, port);
-        // strcat(cacheIdx, resource);
+        // strcat(cacheIdx, query);
 
+        fprintf(stderr, "NEW REQUEST:\n %s", request);
         return 0;
     }
 
     return -1;
 }
 
-int parseRequest(char* str, char* method, char* protocol, char* version, char* hostPort, char* resource) {
+int parseRequest(char* buf, char* method, char* protocol, char* version, char* hostAndPort, char* query) {
 	char url[MAXBUF];
 
-    //  requests with multiple lines
-	if((!strstr(str, MULTILINE_FLAG)) || !strlen(str)) {
+    //  Make sure it is a valid request
+	if((!strstr(buf, FORWARD_SLASH)) || !strlen(buf)) {
 		return -1;
     }
 
-	strcpy(resource, MULTILINE_FLAG);
-	sscanf(str,"%s %s %s", method, url, version);
+    // Extract the method, url and http version
+	sscanf(buf,"%s %s %s", method, url, version);
 
-	if (strstr(url, "://")) {
-		sscanf(url, "%[^:]://%[^/]%s", protocol, hostPort, resource);
+    // Prepend the '/' to the query
+    strcpy(query, FORWARD_SLASH);
+	if (strstr(url, HTTP_DELIM)) {
+        // Extract the http protocol, host:port and query
+		sscanf(url, "%[^:]://%[^/]%s", protocol, hostAndPort, query);
     }
 	else {
-		sscanf(url, "%[^/]%s", hostPort, resource);
+        // No protocol mentioned
+		sscanf(url, "%[^/]%s", hostAndPort, query);
     }
 
 	return 0;
@@ -293,7 +288,17 @@ int forwardToServer(char* host, char* port, int* serverFd, char* requestStr) {
         }
     }
 
-    if (Rio_writen(*serverFd, requestStr, strlen(requestStr)) == -1) {
+    int numBytes = Rio_writen(*serverFd, requestStr, strlen(requestStr));
+    if (numBytes == -1) {
+        return -1;
+    }
+
+    return numBytes;
+}
+
+
+int forwardToClient(int clientFd, char* response, unsigned int len) {
+    if (Rio_writen(clientFd, response, len) == -1) {
         return -1;
     }
 
@@ -301,76 +306,76 @@ int forwardToServer(char* host, char* port, int* serverFd, char* requestStr) {
 }
 
 
-int forwardToClient(int clientFd, char* content, unsigned int len) {
-    if (Rio_writen(clientFd, content, len) == -1) {
-        return -1;
-    }
-
-    return 0;
-}
-
-
-int readAndForwardResponse(int serverFd, int clientFd, char* cacheIdx, char* content) {
+int readAndForwardToClient(int serverFd, int clientFd, char* cacheIdx, char* response) {
     rio_t rioServer;
     char buf[MAXBUF];
-    unsigned int size =0, length = 0, cacheSize =0;
-    int validSize = 1;
+    unsigned int contentLength = 0, cacheSize =0;
+    int validReponseLength = 1;
 
-    content[0] = '\0';
+    response[0] = END_STR;
 
     Rio_readinitb(&rioServer, serverFd);
 
-    // Read
     do {
-        if (Rio_readlineb(&rioServer, buf, MAXBUF) == -1) {
+        int numBytesRead = Rio_readlineb(&rioServer, buf, MAXBUF);
+        // Read the response from the server line by line
+        if (numBytesRead == -1) {
             return -1;
         }
 
-        if (validSize) {
-            validSize = append(content, buf, MAXBUF, &cacheSize);
+        // Append the read bytes to the proxy's response
+        if (validReponseLength) {
+            validReponseLength = append(response, buf, strlen(buf), &cacheSize);
         }
 
-        if (strstr(buf, CONTENT_LEN_STR_2)) {
-			sscanf(buf, "Content-length: %d", &size);
+        // Standardize the content length header
+        if (strstr(buf, CONTENT_LEN_STR_1) || strstr(buf, CONTENT_LEN_STR_2)) {
+			sscanf(buf, "Content-length: %d", &contentLength);
         }
 
-		if (strstr(buf, CONTENT_LEN_STR_1)) {
-			sscanf(buf, "Content-Length: %d", &size);
-        }
-
-        if (Rio_writen(clientFd, buf, length) == -1) {
+        // Write the read bytes to the client
+        if (Rio_writen(clientFd, buf, strlen(buf)) == -1) {
             return -1;
         }
 
-    } while (strcmp(buf, END) != 0 && strlen(buf));
+    } while (strcmp(buf, END_HTTP) != 0 && strlen(buf));
 
-    if (size) {
-        while (size > MAXBUF) {
-            if ((length = Rio_readnb(&rioServer, buf, MAXBUF)) == -1) {
+
+    // Write the response body (if any) to the client
+    unsigned int lengthToWrite = 0;
+    if (contentLength) {
+        // If the content length is more than MAXBUF keep writing until it is not
+        while (contentLength > MAXBUF) {
+            if ((lengthToWrite = Rio_readnb(&rioServer, buf, MAXBUF)) == -1) {
                 return -1;
             }
 
-            if (validSize) {
-                validSize = append(content, buf, MAXBUF, &cacheSize);
+            // Append to response
+            if (validReponseLength) {
+                validReponseLength = append(response, buf, MAXBUF, &cacheSize);
             }
 
-            if (Rio_writen(clientFd, buf, length) == -1) {
+            // Write to client
+            if (Rio_writen(clientFd, buf, lengthToWrite) == -1) {
                 return -1;
             }
 
-            size -= MAXBUF;
+            // Decrease the length of the content left to write
+            contentLength -= MAXBUF;
         }
 
-        if (size) {
-            if ((length = Rio_readnb(&rioServer, buf, size)) == -1) {
+        // Finish writing anything that is leftover from the loop above
+        // Bascially anything less than MAXBUF
+        if (contentLength) {
+            if ((lengthToWrite = Rio_readnb(&rioServer, buf, contentLength)) == -1) {
                 return -1;
             }
 
-            if (validSize) {
-                validSize = append(content, buf, size, &cacheSize);
+            if (validReponseLength) {
+                validReponseLength = append(response, buf, contentLength, &cacheSize);
             }
 
-            if (Rio_writen(clientFd, buf, length) == -1) {
+            if (Rio_writen(clientFd, buf, lengthToWrite) == -1) {
                 return -1;
             }
         }
@@ -378,20 +383,20 @@ int readAndForwardResponse(int serverFd, int clientFd, char* cacheIdx, char* con
     }
     else {
 
-        while ((length = Rio_readnb(&rioServer, buf, MAXLINE)) > 0) {
-            if (validSize) {
-                validSize = append(content, buf, length, &cacheSize);
+        while ((lengthToWrite = Rio_readnb(&rioServer, buf, MAXBUF)) > 0) {
+            if (validReponseLength) {
+                validReponseLength = append(response, buf, lengthToWrite, &cacheSize);
             }
 
-            if (Rio_writen(clientFd, buf, length) == -1) {
+            if (Rio_writen(clientFd, buf, lengthToWrite) == -1) {
                 return -1;
             }
         }
     }
 
-    if (validSize) {
+    if (validReponseLength) {
         // Add to cache
-        // if (insert_content_node(cache_list, cache_index, content, cache_size) == -1) {
+        // if (insert_content_node(cache_list, cache_index, response, cache_contentLength) == -1) {
 		// 	return -2;
         // }
 		// printf("insert correct!\n");
@@ -401,25 +406,29 @@ int readAndForwardResponse(int serverFd, int clientFd, char* cacheIdx, char* con
 
 }
 
-void getHostAndPort(char* hostPort, char* host, char* port) {
-    char* buf = strstr(hostPort,":");
+void getHostAndPort(char* hostAndPort, char* host, char* port) {
+    // Split the host and port into "seperate" strings
+    char* buf = strstr(hostAndPort,":");
 	if (buf) {
-		*buf = '\0';
+		*buf = END_STR;
+        // Copy the port
 		strcpy(port, buf + 1);
 	}
 	else {
+        // Give the default port of '80'
 		strcpy(port, DEFAULT_HTTP_PORT);
     }
 
-	strcpy(host, hostPort);
+    // Copy the host
+	strcpy(host, hostAndPort);
 }
 
-int append(char* content, char* str, unsigned int addSize, unsigned int* prevSize) {
-    if(addSize + (*prevSize) > MAX_OBJECT_SIZE) {
+int append(char* response, char* str, unsigned int addSize, unsigned int* prevSize) {
+    if(addSize + (*prevSize) > MAX_CACHE_SIZE) {
         return 0;
     }
 
-	memcpy(content + (*prevSize), str, addSize);
+	memcpy(response + (*prevSize), str, addSize);
 	*prevSize += addSize;
 	return 1;
 }
